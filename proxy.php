32b9a7c49119c580b6649393181280b973f2a844
jeansouza <jeancfsouza@gmail.com>
2015-12-09 11:37:07 -0200

    proxy modification

--- proxy.php
+++ proxy.php
@@ -1,3 +1,3 @@
 <?php
 
-echo file_get_contents($_GET['url'] . '?map=' . $_GET['map'] . '&service=' . $_GET['service'] . '&request=' . $_GET['request'] . '&version=' . $_GET['version']);
+echo file_get_contents($_GET['url'] . $_GET['params']);
