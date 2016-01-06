295034606c529cba583ea9b4e0c738b95475faa8
jeansouza <jeancfsouza@gmail.com>
2016-01-06 17:33:24 -0200

    node js migration

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express'),
     load = require('express-load'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
-    error = require('./middlewares/error'),
+    //error = require('./middlewares/error'),
     app = express(),
     server = require('http').Server(app),
     io = require('socket.io')(server);
@@ -18,32 +18,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-io.use(function(socket, next) {
-  var data = socket.request;
-  cookie(data, {}, function(err) {
-    var sessionID = data.signedCookies[KEY];
-    store.get(sessionID, function(err, session) {
-      if(err || !session) {
-        return next(new Error('Access denied!'));
-      } else {
-        socket.handshake.session = session;
-        return next();
-      }
-    });
-  });
-});
-
 load('controllers')
   .then('routes')
   .into(app);
 
-load('sockets').into(io);
-
-app.use(error.notFound);
-app.use(error.serverError);
+//app.use(error.notFound);
+//app.use(error.serverError);
 
 module.exports = app;
-
-server.listen(3000, function() {
-  console.log("Bdqueimadas running!");
-});
