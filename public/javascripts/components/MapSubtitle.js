01bc9b31a2f4df2a6d073388115ceab79132e413
jeansouza <jeancfsouza@gmail.com>
2016-03-21 16:03:08 -0300

    map subtitle update

--- public/javascripts/components/MapSubtitle.js
+++ public/javascripts/components/MapSubtitle.js
@@ -11,23 +11,50 @@ BDQueimadas.components.MapSubtitle = (function() {
     });
 
     $('#map-subtitle-items').append(elem);
+
+    updateZoomTop(false);
   };
 
   var removeSubtitle = function(layerId) {
     $("#map-subtitle-items > li." + layerId.replace(':', '')).remove();
+
+    updateZoomTop(false);
   };
 
   var showSubtitle = function(layerId) {
     if(!$("#map-subtitle-items > li." + layerId.replace(':', '')).is(":visible"))
       $("#map-subtitle-items > li." + layerId.replace(':', '')).show();
+
+    updateZoomTop(false);
   };
 
   var hideSubtitle = function(layerId) {
     if($("#map-subtitle-items > li." + layerId.replace(':', '')).is(":visible"))
       $("#map-subtitle-items > li." + layerId.replace(':', '')).hide();
+
+    updateZoomTop(false);
   };
 
-  var init = function() {};
+  // new
+
+  var updateZoomTop = function(toggle) {
+    if($('.map-subtitle-toggle').parent().parent().parent().hasClass('collapsed-box') || !toggle)
+      $('#terrama2-map .ol-zoom').animate({ 'top': '221px' }, { duration: 500, queue: false });
+    else
+      $('#terrama2-map .ol-zoom').animate({ 'top': '70px' }, { duration: 500, queue: false });
+  };
+
+  var loadEvents = function() {
+    $('.map-subtitle-toggle').on('click', function() {
+      updateZoomTop(true);
+    });
+  };
+
+  // new
+
+  var init = function() {
+    loadEvents();
+  };
 
   return {
     init: init,
