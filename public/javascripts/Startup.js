63e1cfb4885e77c0f82a125222f50d2ef726b62f
jeansouza <jeancfsouza@gmail.com>
2016-04-12 09:07:54 -0300

    bug fixes

--- public/javascripts/Startup.js
+++ public/javascripts/Startup.js
@@ -6,18 +6,12 @@ var terrama2Interval = window.setInterval(function() {
   if(TerraMA2WebComponents.obj.isComponentsLoaded()) {
 
     TerraMA2WebComponents.webcomponents.MapDisplay.addBaseLayers('bases', 'Camadas Bases');
-    TerraMA2WebComponents.webcomponents.MapDisplay.addCapabilitiesLayers('http://localhost:9095/geoserver/ows?service=WMS&request=getCapabilities', 'http://localhost:9095/geoserver/ows', 'geoserver', 'local', 'Local Server');
+    //TerraMA2WebComponents.webcomponents.MapDisplay.addCapabilitiesLayers('http://localhost:9095/geoserver/ows?service=WMS&request=getCapabilities', 'http://localhost:9095/geoserver/ows', 'geoserver', 'local', 'Local Server');
 
     TerraMA2WebComponents.webcomponents.MapDisplay.disableDoubleClickZoom();
     TerraMA2WebComponents.webcomponents.MapDisplay.addMousePosition();
     TerraMA2WebComponents.webcomponents.MapDisplay.addScale();
 
     clearInterval(terrama2Interval);
-  }
-}, 10);
-
-var bdqueimadasInterval = window.setInterval(function() {
-  if(BDQueimadas.obj.isComponentsLoaded()) {
-    clearInterval(bdqueimadasInterval);
   }
 }, 10);
