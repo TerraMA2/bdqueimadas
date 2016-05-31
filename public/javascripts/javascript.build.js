90670fb9bd8d82ec8aba4650cda92ca75330e4ed
Jean Souza <jeancfsouza@gmail.com>
2016-05-31 18:04:12 -0300

    updating dependencies

--- public/javascripts/javascript.build.js
+++ public/javascripts/javascript.build.js
@@ -2,12 +2,9 @@
   baseUrl: ".",
   name: "Startup",
   out: "bdqueimadas.min.js",
-  paths: {
-    TerraMA2WC: '../externals/TerraMA2WebComponents/javascripts'
-  },
   exclude: [
-    'TerraMA2WC/TerraMA2WebComponents',
-    'TerraMA2WC/components/LayerExplorer.TerraMA2WebComponents',
-    'TerraMA2WC/components/MapDisplay.TerraMA2WebComponents'
+    '../externals/TerraMA2WebComponents/javascripts/TerraMA2WebComponents',
+    '../externals/TerraMA2WebComponents/javascripts/components/LayerExplorer.TerraMA2WebComponents',
+    '../externals/TerraMA2WebComponents/javascripts/components/MapDisplay.TerraMA2WebComponents'
   ]
 })
