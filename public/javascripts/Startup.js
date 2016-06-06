29efaf2eb4e375449b2e30931015f58bb73f2504
Jean Souza <jeancfsouza@gmail.com>
2016-06-06 15:40:02 -0300

    renaming layers

--- public/javascripts/Startup.js
+++ public/javascripts/Startup.js
@@ -21,7 +21,7 @@ requirejs(
     TerraMA2WebComponents.LayerExplorer.init();
     TerraMA2WebComponents.MapDisplay.init();
 
-    if(TerraMA2WebComponents.MapDisplay.addBaseLayers('bases', 'Camadas Bases'))
+    if(TerraMA2WebComponents.MapDisplay.addBaseLayers('bases', 'Planos de Fundo'))
       TerraMA2WebComponents.LayerExplorer.addLayersFromMap('bases', 'terrama2-layerexplorer');
 
     //TerraMA2WebComponents.MapDisplay.addCapabilitiesLayers('http://localhost:9095/geoserver/ows?service=WMS&request=getCapabilities', 'http://localhost:9095/geoserver/ows', 'geoserver', 'local', 'Local Server', function() {
