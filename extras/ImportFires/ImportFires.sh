6e67eeb4c19364f0db5908a994428eec47090ce0
Jean Souza <jeancfsouza@gmail.com>
2016-06-20 11:15:57 -0300

    import fires script update

---
+++ extras/ImportFires/ImportFires.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#########################################################################################
+###
+### Author: Jean Souza [jean.souza@funcate.org.br]
+### Date: 2016-06-06
+###
+### Purpose: Runs the ImportFires.py script.
+###
+#########################################################################################
+
+python /ImportFires/ImportFires.py
