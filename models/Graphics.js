bb8d9570b287a9efb8e77f647f09e29d6daa9f75
Jean Souza <jeancfsouza@gmail.com>
2016-07-06 16:37:19 -0300

    graphics update

--- models/Graphics.js
+++ models/Graphics.js
@@ -97,7 +97,84 @@ var Graphics = function() {
           query = query.substring(0, (query.length - 1)) + ")";
         }
 
-        query += " group by " + key + " order by count desc;"
+        query += " group by " + key + " order by count desc;";
+
+        // Execution of the query
+        client.query(query, params, function(err, result) {
+          done();
+          if(!err) return callback(null, result);
+          else return callback(err);
+        });
+      } else return callback(err);
+    });
+  };
+
+  /**
+   * Returns the count of the fires.
+   * @param {databaseOperationCallback} callback - Callback function
+   * @returns {databaseOperationCallback} callback - Execution of the callback function, which will process the received data
+   *
+   * @function getFiresTotalCount
+   * @memberof Graphics
+   * @inner
+   */
+  this.getFiresTotalCount = function(dateFrom, dateTo, options, callback) {
+    // Counter of the query parameters
+    var parameter = 1;
+
+    // Connection with the PostgreSQL database
+    memberPg.connect(memberPgConnectionString.getConnectionString(), function(err, client, done) {
+      if(!err) {
+
+        // Creation of the query
+        var query = "select count(*) as count from " + memberTablesConfig.Fires.Schema + "." + memberTablesConfig.Fires.TableName +
+        " where (" + memberTablesConfig.Fires.DateFieldName + " between $" + (parameter++) + " and $" + (parameter++) + ")",
+            params = [dateFrom, dateTo];
+
+        // If the 'options.satellites' parameter exists, a satellites 'where' clause is created
+        if(options.satellites !== undefined) {
+          var satellitesArray = options.satellites.split(',');
+          query += " and " + memberTablesConfig.Fires.SatelliteFieldName + " in (";
+
+          for(var i = 0; i < satellitesArray.length; i++) {
+            query += "$" + (parameter++) + ",";
+            params.push(satellitesArray[i]);
+          }
+
+          query = query.substring(0, (query.length - 1)) + ")";
+        }
+
+        // If the 'options.extent' parameter exists, a extent 'where' clause is created
+        if(options.extent !== undefined) {
+          query += " and ST_Intersects(" + memberTablesConfig.Fires.GeometryFieldName + ", ST_MakeEnvelope($" + (parameter++) + ", $" + (parameter++) + ", $" + (parameter++) + ", $" + (parameter++) + ", 4326))";
+          params.push(options.extent[0], options.extent[1], options.extent[2], options.extent[3]);
+        }
+
+        // If the 'options.countries' parameter exists, a countries 'where' clause is created
+        if(options.countries !== undefined) {
+          var countriesArray = options.countries.split(',');
+          query += " and " + memberTablesConfig.Fires.CountryFieldName + " in (";
+
+          for(var i = 0; i < countriesArray.length; i++) {
+            query += "$" + (parameter++) + ",";
+            params.push(countriesArray[i]);
+          }
+
+          query = query.substring(0, (query.length - 1)) + ")";
+        }
+
+        // If the 'options.states' parameter exists, a states 'where' clause is created
+        if(options.states !== undefined) {
+          var statesArray = options.states.split(',');
+          query += " and " + memberTablesConfig.Fires.StateFieldName + " in (";
+
+          for(var i = 0; i < statesArray.length; i++) {
+            query += "$" + (parameter++) + ",";
+            params.push(statesArray[i]);
+          }
+
+          query = query.substring(0, (query.length - 1)) + ")";
+        }
 
         // Execution of the query
         client.query(query, params, function(err, result) {
